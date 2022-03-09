fec8ea6631f23c85bbca1a9692f12df03ef26a5c
stephen.fabeyo@assentcompliance.com <stephen.fabeyo@assentcompliance.com>
2022-03-09 19:12:41 +0000

    updates

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -6,7 +6,7 @@ paymentmethod,
 status,
 amount/100 as amount,
 created as datecreated
-from {{source('stripe','payments')}}
+from raw.stripe.payment -- {{source('stripe','payments')}}
 )
 select *
 from stg_payments
